84e787a743ea36ce3c1c80ad24960a054f2f79d6
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2024-05-11 17:37:02 +0900

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,12 @@
-import Navs from "./components/Navs";
-import CardBase from "./components/CardBase";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { Post } from "../types/post";
-import { useEffect, useState } from "react";
-import axios from "axios";
+import MyRouter from './router/index';
+import Navs from './components/Navs';
 
 function App() {
-  const [posts, setPosts] = useState<Post[]>([]);
-  useEffect(() => {
-    axios.get("http://127.0.0.1:5000/api/posts").then((res) => {
-      setPosts(res.data);
-    });
-  }, []);
-  const firstPost = posts.length > 0 ? posts[0] : null;
   return (
-    <>
-      <header>
-        <Navs />
-      </header>
-      <main>
-        <div className="container mt-5">
-          <div className="cards">
-          {firstPost && <CardBase post={firstPost} />}
-          </div>
-        </div>
-      </main>
-    </>
+    <div>
+      <Navs />
+      <MyRouter />
+    </div>
   );
 }
 
