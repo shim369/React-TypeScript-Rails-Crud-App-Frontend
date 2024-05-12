85ad71d0ce0a7d07ea1af70a22a17d66add2ccab
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2024-05-12 12:38:30 +0900

    update router

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,13 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 import Post from "../pages/Post";
 import PostDetail from "../pages/PostDetail";
+import PostCreate from "../pages/PostCreate";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function MyRouter() {
   return (
     <Routes>
-      <Route path="/" element={<Post />} />
-      <Route path="/posts/:id" element={<PostDetail />} />
+        <Route path="/" element={<Post />} />
+        <Route path="/posts/:id" element={<PostDetail />} />
+        <Route path="/posts/create" element={<PostCreate />} />
     </Routes>
   );
 }
