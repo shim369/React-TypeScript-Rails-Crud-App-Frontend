7d0ad984373e36a43b3f33e76ffad51403479361
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2024-05-13 18:03:04 +0900

    add delete function

--- src/pages/PostDetail.tsx
+++ src/pages/PostDetail.tsx
@@ -1,16 +1,17 @@
-import { Link, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { PostTypes } from "../../types/postTypes";
 
 function PostDetail() {
+  const navigate = useNavigate();
   let { id } = useParams();
   const [post, setPost] = useState<PostTypes>({
     id: 0,
     title: "",
     content: "",
     image_url: "",
-    image: null
+    image: null,
   });
   const defaultImageUrl = "../logo512.png";
 
@@ -19,6 +20,19 @@ function PostDetail() {
       setPost(res.data);
     });
   }, [id]);
+
+  const handleDelete = () => {
+    if (window.confirm("Delete this post?")) {
+      axios
+        .delete(`http://127.0.0.1:5000/api/posts/${post.id}`)
+        .then(() => {
+          navigate("/");
+        })
+        .catch((error) => {
+          console.error("Error deleting post:", error);
+        });
+    }
+  };
 
   if (!post) {
     return <div>Loading...</div>;
@@ -29,7 +43,6 @@ function PostDetail() {
       <div className="container mt-5">
         <div className="row">
           <div className="col-12 col-md-6 m-auto">
-          <Link to={`/posts/${post.id}/edit`}>Edit</Link>
             <h2>{post.title}</h2>
             <div>
               <img
@@ -39,6 +52,8 @@ function PostDetail() {
               />
             </div>
             <p>{post.content}</p>
+            <Link to={`/posts/${post.id}/edit`} className="btn btn-success">Edit</Link>
+            <button className="btn btn-danger ms-1" onClick={handleDelete}>Delete</button>
           </div>
         </div>
       </div>
